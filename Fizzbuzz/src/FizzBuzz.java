93b1292d21de820f4776d854c24c0c2a6f79c133
rafahg <r.hgar78@gmail.com>
2021-03-22 17:46:57 +0000

    FizzBuz added

--- Fizzbuzz/src/FizzBuzz.java
+++ Fizzbuzz/src/FizzBuzz.java
@@ -1,8 +1,25 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-
+        FizzBuzz myJavaFizz = new FizzBuzz();
+        myJavaFizz.fizzBuzz(100);
     }
 
-    public
+    public static void fizzBuzz(int number) {
+        int n = 0;
+        String value = null;
+        while (n <= number) {
+            if(n % 15 == 0) {
+                value = "fizzbuzz";
+            } else if  (n % 3 == 0) {
+                value = "buzz";
+            } else if (n % 5 == 0) {
+                value = "fizz";
+            } else {
+                value = String.valueOf(n);
+            }
+            n += 1;
+            System.out.println(value);
+        }
+    }
 
 }
